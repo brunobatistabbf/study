3e6ad4dc6031fc74e3d75d3aea8c4d3aab1790d0
Bruno Batista <brunobatistaferreirabbf@gmail.com>
2022-09-14 15:53:12 -0300

    data and operations

---
+++ dataAndOperations/list.py
@@ -0,0 +1,24 @@
+#listas
+#semelhante ao array em outras linguagens
+
+#lista vazia
+list = []
+#para acessar a lista use o id de posição ex: list[1]
+
+kay = ["K", "A", "Y"]
+print(kay)
+i = 0
+for x in kay:
+    print(kay[i])
+    i = i + 1
+
+#primeiro elemento é 0
+#operações com lista
+
+list2 = [1,3,4,6,4,7,8,2,3]
+
+print(sum(list2))
+print(min(list2))
+print(max(list2))
+print(list2[0])
+print(list2[-1])
