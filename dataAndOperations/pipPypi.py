39dacf9eb948acc7912ba2781e9e637be4df2414
Bruno Batista <brunobatistaferreirabbf@gmail.com>
2022-09-25 01:35:14 -0300

    pip and pypi

---
+++ dataAndOperations/pipPypi.py
@@ -0,0 +1,20 @@
+#pip é um gerenciador de pacotes para python
+#Você o pip para instalar modulos
+
+#Modulo é codigo na forma de funções ou objetos
+
+#Pypi é o indice de pacotes oficiais do python
+#pip install <modulename>
+
+#virtualenv
+#para instalar pacotes em um ambiente separado
+#virtualenv name
+#cd name
+#source bin/activate
+
+#pip install package
+
+
+#Search
+#Para pesquisar modulos no Pypi
+#pip search topic
