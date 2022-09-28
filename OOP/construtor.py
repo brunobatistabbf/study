0e54c995850abbac08a28225d59eaa8b2e2aa3b5
Bruno Batista <brunobatistaferreirabbf@gmail.com>
2022-09-27 22:36:38 -0300

    constructor

---
+++ OOP/construtor.py
@@ -0,0 +1,10 @@
+#O construtor é chamado quando um objeto é criado
+#O construtor é criado com a função init
+#Self refere-se a si mesmo o objeto
+
+#start > create Obj > call class constructor >return
+
+#init(self) constroi seu objeto
+
+#Default Value
+
