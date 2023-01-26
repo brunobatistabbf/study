329b50bfdd2bbf161892318256f4c68a361488e5
Bruno Batista <brunobatistaferreirabbf@gmail.com>
2023-01-26 16:07:51 -0300

    basics concepts

---
+++ udemyCourse/introductionsConcepts/whileElse.py
@@ -0,0 +1,11 @@
+"""while/Else  while tem else"""
+i = 0
+string = 'bruno'
+while i < len(string):
+    letra = string[i]
+
+    print(letra)
+    i += 1
+else:
+    print("While executado")
+print("Fora do while")
