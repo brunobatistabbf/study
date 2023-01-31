04af8e377c90b7d711049e5e5b07bcf404589933
Bruno Batista <brunobatistaferreirabbf@gmail.com>
2023-01-31 15:12:41 -0300

    basics concepts

---
+++ udemyCourse/introductionsConcepts/forRange.py
@@ -0,0 +1,18 @@
+"""
+For + Range
+
+"""
+
+numeros =  range(10)
+numeros = range(5, 10)
+numeros = range(0, 100, 2)#O terceiro pula de dois em dois
+
+
+for numero in numeros:
+    print(numero)
+
+
+
+
+
+print(numeros)
