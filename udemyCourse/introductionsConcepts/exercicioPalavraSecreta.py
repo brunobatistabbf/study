04ef37722903d0d108fb2778c2478f88aef14714
Bruno Batista <brunobatistaferreirabbf@gmail.com>
2023-02-04 17:25:58 -0300

    exercise

--- udemyCourse/introductionsConcepts/exercicioPalavraSecreta.py
+++ udemyCourse/introductionsConcepts/exercicioPalavraSecreta.py
@@ -14,3 +14,36 @@ na palavra secreta.
 Faça a contagem de tentativas do seu
 usuário.
 """
+palavra_secreta = 'bruno'
+letra_acertadas = ''
+numero_tentativas = 10
+tentativas = 0 #Resolver bugs de tentativas
+
+while True:
+    numero_tentativas -= 1
+    letra_digitada = input("Digite uma letra")
+
+
+    if len(letra_digitada) > 1:
+        print("Digite apenas uma letra")
+        continue
+
+    if letra_digitada in palavra_secreta:
+        letra_acertadas += letra_digitada
+
+    palavra_formada = ''
+    for letra_secreta in palavra_secreta:
+        if letra_secreta in letra_acertadas:
+            palavra_formada += letra_secreta
+        else:
+            palavra_formada += '*'
+
+    if palavra_secreta == palavra_formada:
+        print("\n \n Você ganhou!!! Parabéns")
+        print(f"A palavra secreta era {palavra_secreta}")
+        break
+
+
+    print(palavra_formada)
+
+print("Jogo Fechado")
