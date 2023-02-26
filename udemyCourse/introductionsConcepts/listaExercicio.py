648a82eccaa395712410b8adad91371184e464a4
Bruno Batista <brunobatistaferreirabbf@gmail.com>
2023-02-26 19:22:59 -0300

    exercise not finished

---
+++ udemyCourse/introductionsConcepts/listaExercicio.py
@@ -0,0 +1,18 @@
+"""
+Faça uma lista de compra
+O usuario pode adicionar, apagar e listar
+"""
+
+lista=[]
+while True:
+    print("Selecione uma opção: \n")
+    opcao = input('[i]Inserir [a]Apagar [l]listar: \n')
+
+    if opcao == 'i':
+        ...
+    elif opcao == 'a':
+        ...
+    elif opcao == 'l':
+        ...
+    else:
+        ...
