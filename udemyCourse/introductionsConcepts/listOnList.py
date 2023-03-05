5fb3cdca356e4a5c0865c171161f4fb32154efab
Bruno Batista <brunobatistaferreirabbf@gmail.com>
2023-03-04 21:43:32 -0300

    interaveis

---
+++ udemyCourse/introductionsConcepts/listOnList.py
@@ -0,0 +1,17 @@
+"""
+Um iteravel dentro do outro
+"""
+
+salas = [
+    ["Bruno Fagner", "Bruno Batista"],
+    ["Carlos Eduardo", "Carlos Rodrigues"],
+    ["Guilherme Dutra", "Guilherme Richard", "Carlos Alberto de Nobrega", (1 , 2, 3, 4, 5)],
+
+]
+
+print(salas[1][0])
+print(salas[0][1])
+print(salas[2][3][3])
+
+for sala in salas:
+    print(sala)
