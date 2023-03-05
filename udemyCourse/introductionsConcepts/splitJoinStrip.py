770639497e4f73f61a7731e13d30f4dc118c3d43
Bruno Batista <brunobatistaferreirabbf@gmail.com>
2023-03-04 21:20:15 -0300

    split join strip

---
+++ udemyCourse/introductionsConcepts/splitJoinStrip.py
@@ -0,0 +1,14 @@
+"""
+Split  - divide uma string
+join -  Une uma string
+strip - corta espaço do começo e do fim
+"""
+#split
+frase = "Que olhar mais lindo"
+lista_palavras = frase.split()
+print(lista_palavras)
+
+
+#join
+frase_unida = ' '.join(lista_palavras)
+print(lista_palavras)
