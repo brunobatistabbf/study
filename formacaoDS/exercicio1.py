1909fefe62c2c3381021d000050921277ce79f96
Bruno Batista <brunobatistaferreirabbf@gmail.com>
2023-08-07 15:29:05 -0300

    exercises DS

---
+++ formacaoDS/exercicio1.py
@@ -0,0 +1,11 @@
+#Faça um programa que tenha uma função chamada amplitude.
+# A função deve receber uma lista e imprimir a amplitude.
+# Crie também um código para testar sua função
+
+
+def amplitude():
+    ...
+
+
+
+amplitude()
---
+++ formacaoDS/exercicio2.py
@@ -0,0 +1,9 @@
+#Faça uma função que receba uma string e imprima esta string na forma vertical
+#por exemplo, se receber python, deve imprimir
+#p
+#y
+#t
+#h
+#o
+#N
+#Uma string do python funciona como uma lista!
