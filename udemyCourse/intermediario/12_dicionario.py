52d0ab38e8952b6cc37ccf1b3b51ed46b0e03e61
Bruno Batista <brunobatistaferreirabbf@gmail.com>
2024-08-06 17:58:57 -0300

    updating

---
+++ udemyCourse/intermediario/12_dicionario.py
@@ -0,0 +1,23 @@
+"""
+Dicionarios são estruturas de dados do tipo par de "chave" e "Valor"
+
+Utiliza-se {} ou a classe Dict
+"""
+pessoa = {
+    'nome': 'Luiz Otávio',
+    'sobrenome': 'Miranda',
+    'idade': 18,
+    'altura': 1.8,
+    'endereços': [
+        {'rua': 'tal tal', 'número': 123},
+        {'rua': 'outra rua', 'número': 321},
+    ],
+}
+# print(pessoa, type(pessoa))
+print(pessoa['nome'])
+print(pessoa['sobrenome'])
+
+print()
+
+for chave in pessoa:
+    print(chave, pessoa[chave])
